ff975a03a15e0a7c69287783c3216d923f380c3b
Todor Videv <videvt@gmail.com>
2021-03-31 12:40:28 +0300

    Restructuring, a new lib project
    Restructured parts of the code, introduced a new lib project, member of the workspace, it is going to be referenced by all the other binary projects. The first thing that was moved there is the shared functionality for poisson-disc sampling.

--- poisson_disc_sampling/src/main.rs
+++ poisson_disc_sampling/src/main.rs
@@ -1,107 +1,29 @@
-use nannou::geom::Rect;
-use nannou::math::cgmath::MetricSpace;
-use nannou::{
-    event::Update,
-    geom::{Range, Vector2},
-    prelude::{BLACK, BLUE, GREEN, PINK, RED},
-    App, Frame,
-};
-use ndarray::{s, Array, Ix2};
-use rand::{self, distributions::uniform::SampleUniform, random, Rng};
-use std::cmp::{max, min};
-use std::f32::consts::TAU;
-use std::ops::Add;
+use doodles_lib::poisson_disc::{self, PoissonDiscSampler};
+use nannou::prelude::*;
 
 const WINDOW_WIDTH: u32 = 1024;
 const WINDOW_HEIGHT: u32 = 640;
-const N: u8 = 2;
+const REJECTION_LIMIT: u8 = 30;
+const MINIMUM_RADIUS: f32 = 3.0;
 
 struct Model {
-    window_rect: Rect,
     r: f32,
-    k: u8,
-    cell_size: u32,
-    background_grid: Array<Option<Vector2>, Ix2>,
-    active_list: Vec<Vector2>,
+    poisson_disc_sampler: PoissonDiscSampler,
+    poisson_sampled_points: Vec<Point2>,
 }
 
 impl Model {
     fn new(
-        window_rect: Rect,
         r: f32,
-        k: u8,
-        cell_size: u32,
-        background_grid: Array<Option<Vector2>, Ix2>,
-        active_list: Vec<Vector2>,
+        poisson_disc_sampler: PoissonDiscSampler,
+        poisson_sampled_points: Vec<Point2>,
     ) -> Self {
         Self {
-            window_rect,
             r,
-            k,
-            cell_size,
-            background_grid,
-            active_list,
+            poisson_disc_sampler,
+            poisson_sampled_points,
         }
     }
-}
-
-fn pick_random_point<S>(x: Range<S>, y: Range<S>) -> Vector2<S>
-where
-    S: PartialOrd + SampleUniform,
-{
-    let mut rng = rand::thread_rng();
-
-    Vector2::from((
-        rng.gen_range(x.start..=x.end),
-        rng.gen_range(y.start..=y.end),
-    ))
-}
-
-fn pick_random_index<T>(list: &Vec<T>) -> usize {
-    (random::<f32>() * list.len() as f32).floor() as usize
-}
-
-fn pick_random_vector(magnitude_range: Range<f32>) -> Vector2<f32> {
-    let mut rng = rand::thread_rng();
-
-    let magnitude = rng.gen_range(magnitude_range.start..magnitude_range.end);
-
-    Vector2::from_angle(rng.gen_range(0.0..TAU)).with_magnitude(magnitude)
-}
-
-fn convert_coordinate(coordinate: f32, from: Range<f32>, to: Range<f32>) -> f32 {
-    to.start + (coordinate - from.start) * (to.end - to.start) / (from.end - from.start)
-}
-
-fn calculate_grid_indices(window_rect: &Rect, point: &Vector2, cell_size: u32) -> (usize, usize) {
-    let cx = convert_coordinate(
-        point.x,
-        window_rect.x,
-        Range::new(0.0, window_rect.w() - 1.0),
-    );
-    let cy = convert_coordinate(
-        point.y,
-        window_rect.y.invert(),
-        Range::new(0.0, window_rect.h() - 1.0),
-    );
-
-    (
-        (cx / cell_size as f32).floor() as usize,
-        (cy / cell_size as f32).floor() as usize,
-    )
-}
-
-fn insert_point_in_grid(
-    point: Vector2,
-    grid: &mut Array<Option<Vector2>, Ix2>,
-    cell_size: u32,
-    window_rect: &Rect,
-) {
-    let (x_index, y_index) = calculate_grid_indices(window_rect, &point, cell_size);
-
-    println!("{}:{}", grid.shape()[0], grid.shape()[1]);
-
-    grid.slice_mut(s![x_index, y_index]).fill(Some(point));
 }
 
 fn main() {
@@ -123,98 +45,19 @@ fn model(app: &App) -> Model {
         Some(w) => w.rect(),
     };
 
-    let r = 50.0;
-    let k: u8 = 30;
+    let r = poisson_disc::calculate_radius(&window_rect, Some(MINIMUM_RADIUS));
 
-    let cell_size = (r / (N as f32).sqrt()).floor() as u32;
+    let poisson_disc_sampler = PoissonDiscSampler::new(window_rect, r, REJECTION_LIMIT);
 
-    let grid_width = (WINDOW_WIDTH / cell_size) + 1;
-    let grid_height = (WINDOW_HEIGHT / cell_size) + 1;
+    let poisson_sampled_points: Vec<Point2> = vec![];
 
-    let mut background_grid =
-        Array::<Option<Vector2>, Ix2>::from_elem((grid_width as usize, grid_height as usize), None);
-
-    let random_point = pick_random_point(window_rect.x, window_rect.y);
-
-    insert_point_in_grid(random_point, &mut background_grid, cell_size, &window_rect);
-    let active_list = vec![random_point];
-
-    Model::new(window_rect, r, k, cell_size, background_grid, active_list)
-}
-
-fn is_point_valid(
-    window_rect: &Rect,
-    point: &Vector2,
-    grid: &Array<Option<Vector2>, Ix2>,
-    cell_size: u32,
-    r: f32,
-) -> bool {
-    if point.x < window_rect.x.start
-        || point.x > window_rect.x.end
-        || point.y < window_rect.y.start
-        || point.y > window_rect.y.end
-    {
-        return false;
-    }
-
-    let (x_index, y_index) = calculate_grid_indices(window_rect, &point, cell_size);
-
-    let shape = grid.shape();
-
-    let neighbourhood = grid.slice(s![
-        max(x_index as i32 - 1, 0) as usize..=min(x_index as i32 + 1, shape[0] as i32 - 1) as usize,
-        max(y_index as i32 - 1, 0) as usize..=min(y_index as i32 + 1, shape[1] as i32 - 1) as usize
-    ]);
-
-    neighbourhood.iter().all(|&p| match p {
-        None => true,
-        Some(p) => !(p.distance(*point) < r),
-    })
+    Model::new(r, poisson_disc_sampler, poisson_sampled_points)
 }
 
 fn update(_app: &App, model: &mut Model, _update: Update) {
-    if !model.active_list.is_empty() {
-        let index = pick_random_index(&model.active_list);
-
-        let active_point = &model.active_list[index];
-
-        let mut counter: u8 = 0;
-
-        let new_point = loop {
-            counter += 1;
-
-            let v = pick_random_vector(Range::new(model.r, 2.0 * model.r));
-
-            let new_point = active_point.add(v);
-
-            if is_point_valid(
-                &model.window_rect,
-                &new_point,
-                &model.background_grid,
-                model.cell_size,
-                model.r,
-            ) {
-                break Some(new_point);
-            }
-
-            if counter == model.k {
-                break None;
-            }
-        };
-
-        match new_point {
-            None => {
-                model.active_list.remove(index);
-            }
-            Some(p) => {
-                insert_point_in_grid(
-                    p,
-                    &mut model.background_grid,
-                    model.cell_size,
-                    &model.window_rect,
-                );
-                model.active_list.push(p);
-            }
+    if !model.poisson_disc_sampler.is_finished() {
+        if let Some(p) = model.poisson_disc_sampler.sample() {
+            model.poisson_sampled_points.push(p);
         }
     }
 }
@@ -224,42 +67,14 @@ fn view(app: &App, model: &Model, frame: Frame) {
 
     draw.background().color(BLACK);
 
-    for (i, r) in model.background_grid.outer_iter().enumerate() {
-        for p in r.iter() {
-            if let Some(p) = p {
-                match i % 3 {
-                    0 => {
-                        draw.tri().w_h(40.0, 40.0).x_y(p.x, p.y).color(RED);
-                    }
-                    1 => {
-                        draw.rect().w_h(40.0, 40.0).x_y(p.x, p.y).color(GREEN);
-                    }
-                    2 => {
-                        draw.ellipse().w_h(40.0, 40.0).x_y(p.x, p.y).color(BLUE);
-                    }
-                    _ => {}
-                }
-            }
-        }
+    for p in &model.poisson_sampled_points {
+        draw.ellipse()
+            .xy(*p)
+            .w_h(model.r / 2.0, model.r / 2.0)
+            .stroke_color(WHITE)
+            .stroke_weight(1.0)
+            .no_fill();
     }
-
-    //
-    // for (i, p) in model.background_grid.iter().enumerate() {
-    //     if let Some(p) = p {
-    //         match i % 3 {
-    //             0 => {
-    //                 draw.tri().w_h(10.0, 20.0).x_y(p.x, p.y).color(RED);
-    //             }
-    //             1 => {
-    //                 draw.rect().w_h(10.0, 10.0).x_y(p.x, p.y).color(GREEN);
-    //             }
-    //             2 => {
-    //                 draw.ellipse().w_h(10.0, 10.0).x_y(p.x, p.y).color(BLUE);
-    //             }
-    //             _ => {}
-    //         }
-    //     }
-    // }
 
     draw.to_frame(app, &frame)
         .expect("There was a problem drawing the current frame.");
--- poisson_domino/src/main.rs
+++ poisson_domino/src/main.rs
@@ -1,19 +1,25 @@
+use doodles_lib::poisson_disc::{self, PoissonDiscSampler};
 use nannou::prelude::*;
-use poisson_domino::PoissonDiscSampler;
-use rand::Rng;
 
 const WINDOW_WIDTH: u32 = 1024;
 const WINDOW_HEIGHT: u32 = 640;
 const REJECTION_LIMIT: u8 = 30;
+const MINIMUM_RADIUS: f32 = 3.0;
 
 struct Model {
+    current_r: f32,
     poisson_disc_sampler: PoissonDiscSampler,
     poisson_sampled_points: Vec<Point2>,
 }
 
 impl Model {
-    fn new(poisson_disc_sampler: PoissonDiscSampler, poisson_sampled_points: Vec<Point2>) -> Self {
+    fn new(
+        current_r: f32,
+        poisson_disc_sampler: PoissonDiscSampler,
+        poisson_sampled_points: Vec<Point2>,
+    ) -> Self {
         Self {
+            current_r,
             poisson_disc_sampler,
             poisson_sampled_points,
         }
@@ -39,24 +45,21 @@ fn model(app: &App) -> Model {
         Some(w) => w.rect(),
     };
 
-    let mut rng = rand::thread_rng();
+    let working_rect = Rect::from_w_h(300.0f32, 150.0f32);
 
-    let working_rect = Rect::from_w_h(600.0f32, 300.0f32);
-
-    let r = rng.gen_range(0.0..(working_rect.w() * working_rect.h()).log2());
+    let r = poisson_disc::calculate_radius(&working_rect, Some(MINIMUM_RADIUS));
 
     let poisson_disc_sampler = PoissonDiscSampler::new(working_rect, r, REJECTION_LIMIT);
 
     let poisson_sampled_points: Vec<Point2> = vec![];
 
-    Model::new(poisson_disc_sampler, poisson_sampled_points)
+    Model::new(r, poisson_disc_sampler, poisson_sampled_points)
 }
 
 fn update(_app: &App, model: &mut Model, _update: Update) {
     if !model.poisson_disc_sampler.is_finished() {
-        match model.poisson_disc_sampler.sample() {
-            None => {}
-            Some(p) => model.poisson_sampled_points.push(p),
+        if let Some(p) = model.poisson_disc_sampler.sample() {
+            model.poisson_sampled_points.push(p);
         }
     }
 }
@@ -69,7 +72,7 @@ fn view(app: &App, model: &Model, frame: Frame) {
     for p in &model.poisson_sampled_points {
         draw.ellipse()
             .xy(*p)
-            .w_h(5.0, 5.0)
+            .w_h(model.current_r / 2.0, model.current_r / 2.0)
             .stroke_color(WHITE)
             .stroke_weight(1.0)
             .no_fill();
